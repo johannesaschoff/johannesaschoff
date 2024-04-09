0be04836164c38d3b6901ae23806a4902c8f9997
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-04-09 15:54:57 +0200

    Update golf_betting_game.py

--- golf_betting_game.py
+++ golf_betting_game.py
@@ -139,7 +139,7 @@ def app():
             # Display confirmation message
             st.markdown("Your selections have been locked in for this round.")
         
-        st.write("Scores")
+        st.subheader("Scores")
         data = {
             "User 1": ["", "", "", ""],  
             "User 2": ["", "", "", ""], 
